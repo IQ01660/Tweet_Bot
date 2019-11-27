2c2d053fe640f260121373c53584eace86e05a6c
Ikram Gabiyev <ikramgabiyev@vlan425-34.subnet-224.amherst.edu>
2019-11-27 13:27:34 -0500

    my bot tags anyone in a tweet whenever someone else tweets about fruits

--- bot.js
+++ bot.js
@@ -12,55 +12,73 @@ var config = require('./config');
 
 var T = new Twit(config);
 
-/**
- * parameters for getting tweets
- */
-const paramsGet = {
-    q: 'real madrid psg',
-    count: 2 
-};
+//tweetIt();
+//setInterval(tweetIt, 1000 * 20);
 
-/**
- * parameters for posting tweets
- */
-const paramsPost = { 
-    status: '#realmadridpsg ended 2-2 after 90 minutes' 
-};
+//set up user stream
+var stream = T.stream('statuses/filter', { track: ['bananas', 'oranges', 'strawberries'] });
 
-/**
- * callback for T.get()
- * @param {*} err 
- * @param {*} data 
- * @param {*} response 
- */
-const gotData = (err, data, response) =>  {
-    var tweets = data.statuses;
-    for(var i = 0; i < tweets.length; i++)
-    {
-        console.log(tweets[i].text);
-    }
+//anytime someone follows me
+stream.on('tweet', tweetSeen)
+
+function tweetSeen(eventMsg) {
+    var name = eventMsg.user.name;
+    var screenName = eventMsg.user.screen_name; //actual twitter account name
+    tweetIt('@' + screenName + ' , loved your fruit tweet');
 }
 
+function tweetIt(msg) {
+    /**
+     * parameters for getting tweets
+     */
+    const paramsGet = {
+        q: 'real madrid psg',
+        count: 2 
+    };
 
-/**
- * callback for T.post()
- * @param {*} err 
- * @param {*} data 
- * @param {*} response 
- */
-const postedData = (err, data, response) => {
-    if (err)
-    {
-        console.log("something went wrong");
+    /**
+     * parameters for posting tweets
+     */
+    var randomNum = Math.floor(Math.random()*100);
+    const paramsPost = { 
+        status: `${msg}; a random number ${randomNum}`
+    };
+
+    /**
+     * callback for T.get()
+     * @param {*} err 
+     * @param {*} data 
+     * @param {*} response 
+     */
+    const gotData = (err, data, response) =>  {
+        var tweets = data.statuses;
+        for(var i = 0; i < tweets.length; i++)
+        {
+            console.log(tweets[i].text);
+        }
     }
-    else
-    {
-        console.log("tweet posted");
+
+
+    /**
+     * callback for T.post()
+     * @param {*} err 
+     * @param {*} data 
+     * @param {*} response 
+     */
+    const postedData = (err, data, response) => {
+        if (err)
+        {
+            console.log("something went wrong");
+        }
+        else
+        {
+            console.log("tweet posted");
+        }
     }
+
+
+    T.get('search/tweets', paramsGet, gotData);
+
+    T.post('statuses/update', paramsPost, postedData);
+
 }
-
-T.get('search/tweets', paramsGet, gotData);
-
-T.post('statuses/update', paramsPost, postedData);
-
-
